0d1702747d733914c2b68aa2e20e7ade06a11dfb
ThanhSon2903 <thanhnguyensonjqk@gmail.com>
2026-07-07 14:24:44 +0700

    add taildwin css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   define: {
     // Định nghĩa biến global cho các thư viện cũ như sockjs-client kế thừa
     global: 'window', 
